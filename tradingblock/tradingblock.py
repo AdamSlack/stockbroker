f1053e8cbb931cf2be2174ee109699cf469e63ed
Adam Slack <adamslack@outlook.com>
2017-11-15 16:16:24 +0000

    trading block xml handling in python implemented#

--- tradingblock/tradingblock.py
+++ tradingblock/tradingblock.py
@@ -1,31 +1,112 @@
 import xmlschema
 from pprint import pprint
 
-xs = xmlschema.XMLSchema('./companyStockSchema/CompanyStock.xsd')
 
-print(xs.is_valid('./trades/trades.xml'))
+class Trade:
+    """ Trade """
 
-class Trade():
-
-    __init__(companyName, stockID, availableShares, price, owner):
+    def __init__(self, company_name, stock_id, available_shares, price, owner):
         """ Constructor """
-        this.companyName = companyName
-        this.stockID = stockID
-        this.availableShares = availableShares
-        this.price = price
-        this.owner = owner
+        self.company_name = company_name
+        self.stock_id = stock_id
+        self.available_shares = available_shares
+        self.price = price
+        self.owner = owner
 
-class Price():
+    def xmlise(self):
+        """ convert class into xml for writing to file """ 
+        xml = '<trade>'
+        xml += '<companyName>' + self.company_name + '</companyName>'
+        xml += '<stockID>' + self.stock_id + '</stockID>'
+        xml += '<availableShares>' + str(self.available_shares) + '</availableShares>'
+        xml += self.price.xmlise()
+        xml += '<owner>' + self.owner + '</owner>'
+        xml += '</trade>'
+        return xml
 
-    __init__(value, currency):
+
+class Price:
+    """ Price """
+
+    def __init__(self, value, currency):
         """ Constructor """
-        this.value = value
-        this.currecnt = currency
+        self.value = value
+        self.currency = currency
 
-class Trades():
+    def xmlise(self):
+        """ convert class into xml for writing to file """
+        xml = '<price>'
+        xml += '<currency>' + str(self.currency) +'</currency>'
+        xml += '<value>' + str(self.value) + '</value>'
+        xml += '</price>'
+        return xml
 
-    __init__(trades):
+
+class Trades:
+    """ Trades """
+
+    def __init__(self, trades):
         """ Constructor """
-        this.trades = trades
+        self.trades = trades
 
-    
+    def xmlise(self):
+        """ convert class into xml for writing to file. """
+        xml = '<?xml version="1.0"?> <cs:trades xmlns:cs="CompanyStock">'
+        for trade in self.trades:
+            xml += trade.xmlise()
+        xml += '</cs:trades>'
+        return xml
+
+    def save_trades(self, path):
+        """ XMLise and save file, checking if valid xml bethod hand"""
+        with open(path, 'w') as f:
+            f.write(self.xmlise())
+        
+
+        
+
+class XMLParser:
+    """ XMLParser """
+
+    def __init__(self, schema_path):
+        """ Constructor """
+        self.xs = xmlschema.XMLSchema(schema_path)
+
+    def parse_xml(self, path):
+        """ Parse and Validate an xml file into relevant classes """
+        if self.xs.is_valid(path):
+            print('Valid XML File')
+            xml = self.xs.to_dict(path)
+            trades = []
+            for t in xml['trade']:
+                p = t['price']
+                price = Price(value=p['value'], currency=p['currency'])
+                trade = Trade(
+                    stock_id = t['stockID'],
+                    company_name = t['companyName'],
+                    owner = t['owner'],
+                    available_shares = t['availableShares'],
+                    price = price
+                )
+                trades.append(trade)
+            return Trades(trades)
+        else:
+            print('invalid XML file')
+
+    def validate(self, path):
+        return self.xs.is_valid(path)
+
+def main():
+    """ main """
+    xp = XMLParser('./companyStockSchema/CompanyStock.xsd')
+    trades = xp.parse_xml('./trades/trades.xml')
+    trades.save_trades('./trades/test.xml')
+    if xp.validate('./trades/test.xml'):
+        print('XML Validated Successfully.')
+    else:
+        print('XML Failed Validation')
+
+
+
+if __name__ == '__main__':
+    main()
