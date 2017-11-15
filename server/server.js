43c16b9e51f54cbde2b8886675144cb99484a409
Adam Slack <adamslack@outlook.com>
2017-11-15 11:29:41 +0000

    stock queries returned from alpha vantage

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 'use-strict';
 
 const express = require('express');
-
+var request = require('request');
 const APIKEY = "EN5TVUAQ24R3A67K";
 
 
@@ -14,18 +14,35 @@ stockBroker.get('/', (req, res) => {
 stockBroker.get('/stockbroker', (req, res) => {
     stockID = req.query.stockID;
     granularity = req.query.granularity;
-
-    console.log(req.query.stockID);
-    console.log(req.query.granularity);
+    url = "https://www.alphavantage.co/query?function=" + granularity + "&symbol="+ stockID +"&apikey="+ APIKEY +"&datatype=json";
     
-    response = {
+    console.log(stockID);
+    console.log(granularity);
+    console.log(url);
+
+    results = {
         'stockID': stockID,
         'granularity': granularity,
-        'url' : "https://www.alphavantage.co/query?function=" + granularity + "&symbol="+ companyID +"&apikey="+ APIKEY +"&datatype=json"
-        
+        'url' : url,
+        'data' : '',
+        'err' : ''
+
     }
 
-    res.send(JSON.stringify(response, null, 2));
+    request(url, (error, response, body) => {
+        if (error) {
+            results.err = error;
+        }
+        if (response && response.statusCode) {
+            console.log(response.statusCode);
+        }
+        console.log(body);
+        results.body = JSON.parse(body);
+        res.send(JSON.stringify(results, null, 2));        
+    });
+    
+
 });
 
 stockBroker.listen(8080);
+console.log('Running on: localhost:8080')
