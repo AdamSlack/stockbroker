a60e1b5f9f72daedc9b6ef53fd2fc0fec470ebdc
Adam Slack <adamslack@outlook.com>
2017-11-16 18:17:23 +0000

    client

--- client/StockBrokerClient/src/app/stock-viewer/stock-viewer.component.ts
+++ client/StockBrokerClient/src/app/stock-viewer/stock-viewer.component.ts
@@ -13,14 +13,36 @@ export class StockViewerComponent implements OnInit {
 
   public stock : any = 'STOCK BE HERE';
   public trades : any = 'TRADES BE HERE';
+
+
+  public dates : string[] = [];
+  public open : string[] = [];
+  public high : string[] = [];
+  public low : string[] = [];
+  public close : string[] = [];
+  public volume : string[] = []; 
+
   public query : string;
   public search() {
     this.stockQuery.requestStock(this.query, 'TIME_SERIES_DAILY').then((res) => {
+      let data = res['data'];
+      let data_keys = Object.keys(data);
+      let values = data[data_keys[1]];
+      let stock_data = this.processStockData(values)
       this.stock = JSON.stringify(res['data'], null, 2);
     });
     this.stockQuery.requestTrades(this.query.length != 0 ? {stockID: this.query} : {}).then((res) => {
       this. trades = JSON.stringify(res['data'], null, 2);
     })
+  }
+
+  public processStockData(values : any) {
+    this.dates = Object.keys(values);
+    this.open = this.dates.map((date) => values[date]['1. open']);
+    this.high = this.dates.map((date) => values[date]['2. high']);
+    this.low = this.dates.map((date) => values[date]['3. low']);
+    this.close = this.dates.map((date) => values[date]['4. close']);
+    this.volume = this.dates.map((date) => values[date]['5. volume']);
   }
 
   ngOnInit() {
