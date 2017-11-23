2fc0082770b2c0013bf55151ea0181a1d7781577
Adam Slack <adamslack@outlook.com>
2017-11-23 10:50:50 +0000

    d3 stock figure added

--- client/StockBrokerClient/src/app/stock-viewer/stock-viewer.component.ts
+++ client/StockBrokerClient/src/app/stock-viewer/stock-viewer.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, ElementRef, ViewChild } from '@angular/core';
 
 import { StockQueryService } from '../services/stock-query.service';
 import { Observable, Subscription } from 'rxjs';
-
+import * as d3 from 'd3';
 @Component({
   selector: 'app-stock-viewer',
   templateUrl: './stock-viewer.component.html',
@@ -26,7 +26,23 @@ export class StockViewerComponent implements OnInit {
   public volume : string[] = []; 
 
   public query : string;
-  public search() {
+  
+  @ViewChild('chart') chart: ElementRef;
+  @Input() dataset: Array<{close: number, date: any}>;
+
+  chartWidth: number = 200;
+  chartHeight: number = 200;
+  
+  public buildStockDataset() : void {
+   
+    var cumulative = [];
+    this.dataset = this.close.map((v,idx,) => {
+      return {close: parseFloat(v), date: this.dates[idx]}
+    });
+    console.log(this.dataset);
+  }
+  
+  public search() : void {
 
     this.stockSubscription.unsubscribe();
     this.stockSubscription = this.stockQuery.requestStock(this.query, 'TIME_SERIES_DAILY').subscribe((res) => {
@@ -35,12 +51,74 @@ export class StockViewerComponent implements OnInit {
       let values = data[data_keys[1]];
       let stock_data = this.processStockData(values)
       this.stock = JSON.stringify(res['data'], null, 2);
+
+      this.buildStockDataset();
+      this.buildChart();
+      
     });
 
     this.tradeSubscription.unsubscribe();
     this.tradeSubscription = this.stockQuery.requestTrades(this.query.length != 0 ? {stockID: this.query} : {}).subscribe((res) => {
       this. trades = JSON.stringify(res['data'], null, 2);
     })
+  }
+
+  public buildChart(){
+    if (!this.dataset) {
+      return;
+    }
+    console.log('Building D3 Chart.')
+    var margin = {top: 20, right: 20, bottom: 50, left: 50},
+        width = 750 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;
+
+    var svg = d3.select(this.chart.nativeElement)
+    svg.selectAll('*').remove();
+    svg.attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform",
+              "translate(" + margin.left + "," + margin.top + ")");
+             
+    var g = svg.append('g').attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
+
+  var x = d3.scaleTime()
+      .rangeRound([0, width]);
+
+  var y = d3.scaleLinear()
+      .rangeRound([height, 0]);
+  var parseTime = d3.timeParse('%Y-%m-%d');
+      
+  var line = d3.line()
+      .x((d) => { return x(parseTime(d.date)); })
+      .y((d) => { return y(d.close); });
+
+  x.domain(d3.extent(this.dataset, (d) => { return parseTime(d.date)}));
+  y.domain([0, d3.max(this.dataset, (d) => { return d.close; })]);
+  
+  g.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x))
+    .select(".domain")
+
+  g.append("g")
+      .call(d3.axisLeft(y))
+    .append("text")
+      .attr("fill", "#000")
+      .attr("transform", "rotate(-90)")
+      .attr("y", 6)
+      .attr("dy", "0.71em")
+      .attr("text-anchor", "end")
+      .text("Price ($)");
+
+    g.append("path")
+        .datum(this.dataset)
+        .attr("fill", "none")
+        .attr("stroke", "steelblue")
+        .attr("stroke-linejoin", "round")
+        .attr("stroke-linecap", "round")
+        .attr("stroke-width", 1.5)
+        .attr("d", line);
   }
 
   public processStockData(values : any) {
@@ -55,6 +133,7 @@ export class StockViewerComponent implements OnInit {
   ngOnInit() {
     this.stockSubscription = this.stockQuery.requestStock('', 'TIME_SERIES_DAILY').subscribe();
     this.tradeSubscription = this.stockQuery.requestTrades(''.length != 0 ? {stockID: ''} : {}).subscribe();
+    this.buildChart();
   }
 
 }
